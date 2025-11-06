8b380c1aa2daa949f72461df428631e14b79c55a
Pablo Rodrigo <pablinho.pres@hotmail.com>
2025-11-05 21:43:25 -0300

    ajustes config para hostinger

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Site_Tenorio_2025/", // Caminho correto para GitHub Pages
+  base: "/", 
 });
